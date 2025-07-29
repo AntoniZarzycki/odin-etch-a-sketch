9be1826bda8eafc435acc36142e96cbfbbff4129
Antoni Zarzycki <145159295+AntoniZarzycki@users.noreply.github.com>
2025-07-29 15:24:59 +0200

    feat: add a function to generate pixels on the canvas

---
+++ script.js
@@ -0,0 +1,13 @@
+function generatePixels(count) {
+  const canvas = document.querySelector("#canvas");
+  const size = canvas.offsetWidth / count;
+
+  for (let i = 1; i <= count * count; i++) {
+    const pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+    pixel.setAttribute("style", `width: ${size}px; height: ${size}px;`);
+    canvas.appendChild(pixel);
+  }
+}
+
+generatePixels(16);
